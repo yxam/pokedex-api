897aa1ceeceb573ce3c6da9f831f1c004507453b
yxam <mbeltraoyarzun@gmail.com>
2024-10-29 10:37:31 -0300

    feat: Add more swagger properties

--- src/pokemon/infrastructure/pokemon.controller.ts
+++ src/pokemon/infrastructure/pokemon.controller.ts
@@ -1,18 +1,30 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { FindOrCreatePokemonDto } from '../application/dtos';
 import { FindOrCreatePokemonUseCase } from '../application/uses-cases/FindOrCreatePokemon.useCase';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { FindOrCreatePokemonResponseDto } from '../application/dtos/find-or-create-pokemon-response.dto';
+import { ErrorResponseDto } from 'src/core/application/dtos/error-response.dto';
 
-@Controller('pokemons')
+@ApiTags('Pokemon')
+@Controller('pokemon')
 export class PokemonController {
   constructor(
-    private readonly findOrCreatePokemonUseCase: FindOrCreatePokemonUseCase,
+    private readonly findOrCreatePokemonUseCase: FindOrCreatePokemonUseCase
   ) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @ApiResponse({ type: FindOrCreatePokemonResponseDto })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    type: FindOrCreatePokemonResponseDto,
+    description: 'Created',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    type: ErrorResponseDto,
+    description: 'Pokemon not found',
+  })
   findOrCreate(@Body() dto: FindOrCreatePokemonDto) {
     return this.findOrCreatePokemonUseCase.execute(dto);
   }
